2d387dfa0bdf25a55e6afae0cc460b6d4438bddd
ParidaMAMAT <101190162+paridaMamat@users.noreply.github.com>
2023-10-13 11:20:16 +0200

    Update HumanB.cpp

--- ex03/srcs/HumanB.cpp
+++ ex03/srcs/HumanB.cpp
@@ -2,7 +2,7 @@
 #include "../includes/HumanB.hpp"
 
 
-HumanB :: HumanB(std::string name) :_name(name){
+HumanB :: HumanB(std::string name) :_name(name), _weapon(nullptr) {
 
 }
 HumanB :: HumanB(std::string name, Weapon &weapon) : _name(name), _weapon(&weapon){
@@ -10,8 +10,13 @@ HumanB :: HumanB(std::string name, Weapon &weapon) : _name(name), _weapon(&weapo
 }
 void HumanB :: attack()
 {
-    std::cout << _name << " attack with thier " << _weapon->getType() << std::endl;
+    if (weapon)
+        std::cout << _name << " attack with thier " << _weapon->getType() << std::endl;
+    else
+       std::cout << _name << " attack with nothing " << std::endl;
+
 }
+
 void    HumanB :: setWeapon(Weapon &newWeapon){
     _weapon = &newWeapon;
 }
